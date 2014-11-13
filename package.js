7e30b1e8d53aa594b46dc75690576b558fd1a51b
Matt Crider <mattcrider@gmail.com>
2014-11-12 21:16:10 -0500

    Fixes for Meteor 1.0

--- package.js
+++ package.js
@@ -1,36 +1,36 @@
 Package.describe({
-    summary: "Azimuth CMS frontend templates (using Bootstrap 3.x)"
+  name: 'mcrider:azimuth-views-bootstrap',
+  summary: 'Azimuth CMS frontend templates (using Bootstrap 3.x)',
+  version: '0.3.7',
+  git: 'https://github.com/mcrider/azimuth-views-bootstrap'
 });
 
 Package.on_use(function (api) {
-  api.use(['less', 'templating', 'bootstrap-3'], 'client');
-  api.use(['azimuth-core'], 'client', {unordered: true});
+  api.use(['less', 'templating', 'mizzao:bootstrap-3'], 'client');
+  api.use('mcrider:azimuth-core', {unordered: true});
 
-  api.add_files([
-    'css/style.less',
-    'blocks/four_column/four_column.html',
-    'blocks/six_column/six_column.html',
-    'blocks/three_column/three_column.html',
-    'blocks/twelve_column/twelve_column.html',
-    'blocks/two_column/two_column.html',
-    'pages/home_page/home_page.html',
-    'pages/page_default/page_default.html',
-    'pages/sidebar_left/sidebar_left.html',
-    'pages/sidebar_left_fixed/sidebar_left_fixed.html',
-    'pages/sidebar_right/sidebar_right.html',
-    'views/404.html',
-    'views/block_display.html',
-    'views/footer.html',
-    'views/header.html',
-    'views/layout.html',
-    'views/not_authorized.html',
-    'views/accounts/account_buttons.html',
-    'views/accounts/error.html',
-    'views/accounts/forgot_password.html',
-    'views/accounts/login.html',
-    'views/accounts/sign_up.html',
-    'views/accounts/social.html',
-    'lib/utils.js'
-  ], 'client');
+  api.add_files('css/style.less', 'client');
+  api.add_files('blocks/four_column/four_column.html', 'client');
+  api.add_files('blocks/six_column/six_column.html', 'client');
+  api.add_files('blocks/three_column/three_column.html', 'client');
+  api.add_files('blocks/twelve_column/twelve_column.html', 'client');
+  api.add_files('blocks/two_column/two_column.html', 'client');
+  api.add_files('pages/home_page/home_page.html', 'client');
+  api.add_files('pages/page_default/page_default.html', 'client');
+  api.add_files('pages/sidebar_left/sidebar_left.html', 'client');
+  api.add_files('pages/sidebar_left_fixed/sidebar_left_fixed.html', 'client');
+  api.add_files('pages/sidebar_right/sidebar_right.html', 'client');
+  api.add_files('views/404.html', 'client');
+  api.add_files('views/block_display.html', 'client');
+  api.add_files('views/footer.html', 'client');
+  api.add_files('views/header.html', 'client');
+  api.add_files('views/layout.html', 'client');
+  api.add_files('views/not_authorized.html', 'client');
+  api.add_files('views/accounts/account_buttons.html', 'client');
+  api.add_files('views/accounts/error.html', 'client');
+  api.add_files('views/accounts/forgot_password.html', 'client');
+  api.add_files('views/accounts/login.html', 'client');
+  api.add_files('views/accounts/sign_up.html', 'client');
+  api.add_files('views/accounts/social.html', 'client');
+  api.add_files('lib/utils.js', 'client');
 });
-
